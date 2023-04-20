add2b5dd328239f7e0ab748a579dc0ea8df6d584
Ali Parlakci <parlakciali@gmail.com>
2023-04-20 16:56:04 +0300

    Initial commit

---
+++ src/test/java/com/parlakci/karadelikbank/KaradelikBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parlakci.karadelikbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaradelikBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
